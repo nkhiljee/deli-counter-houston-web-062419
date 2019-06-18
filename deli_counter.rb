a77ff9b5666b71518df03b8ec912c6d51986abe8
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-18 03:33:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,13 @@
 # Write your code here.
 katz_deli = [];
 
+def line 
+  if katz_deli.length == 0 
+    puts "the line is currently empty."
+  else
+    puts "your current number is #{katz_deli.length}."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
