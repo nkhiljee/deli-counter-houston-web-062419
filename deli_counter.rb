48b31c3725aeb4a101d1e15df07f3e977b55ba0c
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-18 04:08:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,8 @@ def line(array)
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-    
     array.each_with_index do |value, index|
     message += " #{index.to_i+1}. #{value}"
-    
     end
   puts "#{message}"  
   end
