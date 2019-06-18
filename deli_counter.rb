e506d304a32299ba979cc2dac21cc885e632b179
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-18 04:00:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(array)
   if (array.length == 0)
     puts "The line is currently empty."
   else
-    i = 0
+    message = "The line is currently:"
     array.each do
-    puts "The line is currently: " + katz_deli.length + ". " + katz_deli[i]
-    i += 1
+    message += " #{katz_deli.length}. #{katz_deli[i+1]}"
+    
     end
   end
 end
