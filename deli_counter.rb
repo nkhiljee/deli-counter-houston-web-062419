79fc087a4a011cb7a94acf00920cf6b99441770e
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-18 03:43:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,10 @@ def line(katz_deli)
   if (katz_deli.length == 0)
     puts "The line is currently empty."
   else
+    i = 0
     loop
     puts "The line is currently: " + {katz_deli.length} + ". " + {katz_deli}
+    i += 1
   end
 end
 
