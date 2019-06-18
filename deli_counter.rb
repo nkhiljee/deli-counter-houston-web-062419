2585721fb00361a41a47cbe9b63fda0ee875fed9
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-18 03:48:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 katz_deli = [];
 
-def line(katz_deli)
-  if (katz_deli.length == 0)
+def line(array)
+  if (array.length == 0)
     puts "The line is currently empty."
   else
     i = 0
